f2094fc842651235512828f557dca369f75ea3d8
Martin Svejda <svejdam13@gmail.com>
2020-10-01 22:46:13 +0100

    update

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
 
 export class TodoItem extends Component {
+  getStyle = () =>{
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+    }
+  }
+  markComplete = (e) => { //pokud tady nebude arrow fce, budou tam chyby
+    console.log(this.props);
+  }
   render() {
     return (
-      <div>
-        <p>{ this.props.todo.title }</p>
+      <div style={this.getStyle()}> {/*funkce pro styling (metoda v class, proto tam je this) */}
+        <p>
+          <input type="checkbox" onChange={this.markComplete}/> {' '}
+          { this.props.todo.title }
+        </p>
       </div>
     )
   }
+}
+
+// PropTypes
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
 }
 
 export default TodoItem
