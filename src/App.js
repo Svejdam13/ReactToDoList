d9f588d6e3b05541ce408d2d05850856ec9c5106
Martin Svejda <svejdam13@gmail.com>
2020-10-06 20:10:32 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,64 +2,72 @@ import React, { Component } from 'react'
 import Header from './components/layout/Header';
 import Todos from './components/todos/Todos';
 import AddTodo from './components/todos/AddTodo';
-import uuid from 'uuid'; // To si musel nainstalovat
+import { v4 as uuidv4 } from "uuid"; 
 import './App.css';
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: uuid.v4(),
-        title: 'Take out the trash',
-        completed: false
+        id: uuidv4(), //volas funkci co je v importu (npm install uuid) generujes tim id
+        title: "Take out the trash",
+        completed: false,
       },
       {
-        id: uuid.v4(),
-        title: 'Dinner for visitors',
-        completed: false
+        id: uuidv4(),
+        title: "Dinner for visitors",
+        completed: false,
       },
       {
-        id: uuid.v4(),
-        title: 'Finish app',
-        completed: false
-      }
-    ]
-  }
+        id: uuidv4(),
+        title: "Finish app",
+        completed: false,
+      },
+    ],
+  };
 
   // Toggle Complete
   markComplete = (id) => {
-    this.setState({todos: this.state.todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo;
-    }) });
-  }
-// Delete Todo
-delTodo = (id) => {
-  this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id) ]});
-}
-// Add Todo
-addTodo = (title) => {
-  const newTodo = {
-    id: uuid.v4(),
-    title,
-    completed: false
-  }
-  this.setState({ todos: [...this.state.todos, newTodo] });
-}
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+  // Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
 
-  render(){
-  return (
-    <div className="App">
-      <div className="container">
-        <Header />
-        <AddTodo addTodo={this.addTodo}/>
-        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
       </div>
-    </div>
-  );
- }
+    );
+  }
 }
 
 export default App;
